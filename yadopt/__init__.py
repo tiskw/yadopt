79923d359784de0947f4d0c5ccbec0cb6d70e1bc
Tetsuya Ishikawa <tiskw111@gmail.com>
2024-11-24 12:03:04 +0900

    Update: version

--- yadopt/__init__.py
+++ yadopt/__init__.py
@@ -7,7 +7,7 @@ from .yadopt import parse, wrap, to_dict, to_namedtuple, save, load
 from .gendat import YadOptArgs
 
 # Version information.
-__version__ = "2024.11.22"
+__version__ = "2024.11.24"
 
 # Declare published functions and variables.
 __all__ = ["parse", "wrap", "to_dict", "to_namedtuple", "save", "load", "YadOptArgs", "__version__"]
